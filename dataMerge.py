fa239ddf5c75569cc879619e417192320b1b60b9
Edward <13683373608@163.com>
2022-04-11 19:20:34 +0800

    dataMerge module

--- dataMerge.py
+++ dataMerge.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 from position import *
  
-os.chdir('C:/Users/jrqh/Desktop/test/data')
+os.chdir('C:/Users/ZJJ/Desktop/test/data')
 fileDir = os.getcwd()
 # get all file name
 CsvList = []
@@ -15,14 +15,14 @@ for root,dirs,files in os.walk(fileDir):
             CsvList.append(file)
 
 data = pd.DataFrame()
-data0 = pd.read_csv('C:/Users/jrqh/Desktop/test/10003549.csv',usecols=[1,2,6,7,9,10])
+data0 = pd.read_csv('C:/Users/ZJJ/Desktop/test/10003549.csv',usecols=[1,2,6,7,9,10])
 
 numCsvList = len(CsvList)
 for i in list(range(numCsvList)):
     datai = pd.read_csv(CsvList[i],usecols=[1,2,6,7,9,10])
     datai.rename(columns={'thscode':'thscode%s'%i,'close':'close%s'%i,'DOM':'DOM%s'%i,'deltacash':'deltacash%s'%i,'delta':'delta%s'%i}, inplace=True) 
     data0 = pd.merge(data0,datai,on='time')
-data0.to_csv('ALL2.csv',index=0)
+data0.to_csv('dataMerge.csv',index=0)
 
 
  
